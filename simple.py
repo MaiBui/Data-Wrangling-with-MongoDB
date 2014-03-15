bb45fe98969d6f9c59a3a62a0a866dff73d01a23
tetronoz <tetron@gmail.com>
2014-03-15 12:19:04 +0400

    First programming quiz

--- simple.py
+++ simple.py
@@ -16,9 +16,22 @@ DATAFILE = "beatles-diskography.csv"
 def parse_file(datafile):
     data = []
     with open(datafile, "rb") as f:
-        for line in f:
-            print line
+        header = f.readline().split(",")
 
+        line_counter = 0
+        for line in f:
+            if line_counter == 10:
+                break
+                
+            fv = line.split(",")
+            e = {}
+
+            for i,v in enumerate(fv):
+                e[header[i].strip()] = v.strip()
+
+            data.append(e)
+            line_counter += 1
+                     
     return data
 
 
@@ -28,6 +41,7 @@ def test():
     d = parse_file(datafile)
     firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
     tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+    
 
     assert d[0] == firstline
     assert d[9] == tenthline
